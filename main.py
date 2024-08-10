25d397754ada9a6c13faee72d9e0e1c6f7434b32
Frei-code <felixgzzcas@gmail.com>
2024-08-10 16:06:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,61 +4,46 @@ import discord
 from bot_logic import *
 import random
 from discord.ext import commands
-import os
-import requests
-print(os.listdir('images'))
 
 
+# Configuración del bot de Discord
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='$', intents=intents)
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hola, soy un bot {bot.user}!')
-@bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
-@bot.command()
-async def mem(ctx):
-    with open('images/c.jpg', 'rb') as f:
-        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
-        picture = discord.File(f)
-    # A continuación, podemos enviar este archivo como parámetro.
-    await ctx.send(file=picture)
+intents.guilds = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
+# Diccionario para almacenar datos financieros
+finances = {}
+
+# Función para añadir usuario
+def add_user(user_id):
+    if user_id not in finances:
+        finances[user_id] = {
+            'income': [],
+            'expenses': []
+        }
+
+# Comando para añadir ingresos
 @bot.command()
-async def mem4(ctx):
-    images = os.listdir('images')
-    with open(f'images/{random.choice(images)}', 'rb') as f:
-            picture = discord.File(f)
-    await ctx.send(file=picture)
+async def welcome(ctx):
+    # Mensaje de bienvenida
+    welcome_message = (
+        "¡Bienvenido al servidor!\n"
+        "Aquí están los comandos disponibles:\n"
+    )
 
-def get_duck_image_url():    
-    url = 'https://random-d.uk/api/random'
-    res = requests.get(url)
-    data = res.json()
-    return data['url']
-@bot.command('duck')
-async def duck(ctx):
-    '''Una vez que llamamos al comando duck, 
-    el programa llama a la función get_duck_image_url'''
-    image_url = get_duck_image_url()
-    await ctx.send(image_url)
+    
+    await ctx.send(welcome_message)
 
-def get_duck_image_url():    
-    url = 'https://random-d.uk/api/random'
-    res = requests.get(url)
-    data = res.json()
-    return data['url']
-@bot.command('duck')
-async def duck(ctx):
-    '''Una vez que llamamos al comando duck, 
-    el programa llama a la función get_duck_image_url'''
-    image_url = get_duck_image_url()
-    await ctx.send(image_url)
+    
+
+
+
+
+bot.run(settings["TOKEN"])
+
+# This example requires the 'members' and 'message_content' privileged intents to function.
+
 
 
 
